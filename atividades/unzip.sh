73a9513e7732d2d1d9f13826cd1e8bcaf0555955
Phelps <phelpsmemo@gmail.com>
2019-10-23 01:09:13 -0300

    Correção e pequena descrição no script

--- atividades/unzip.sh
+++ atividades/unzip.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Script para facilitar a extração dos dados
+# Para utilizar este script execute: chmod +x && ./unzip.sh
 
 unzip dados/2_queimadas/1_FocosQueimadas_JaneiroOutubro2019.zip -d dados/2_queimadas/
-unzip dados/3_desmatamento/1_Deter_ate-Outubro2019.zip dados/3_desmatamento/
+unzip dados/3_desmatamento/1_Deter_ate-Outubro2019.zip -d dados/3_desmatamento/
