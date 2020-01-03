0721e5781d9ce02b966b0505f089a87bea62d171
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-01-03 15:18:43 -0300

    Atualização dos comentários

--- src/compile.R
+++ src/compile.R
@@ -1,16 +1,3 @@
-# Pode ser necess�rio alterar os comandos abaixo (Caso seu ambiente esteja
-# # configurado de forma diferente)
-# venvCommand <- 'python -m venv venv';
-# installCommand <- 'venv/bin/pip3 install pandas geopandas matplotlib';
-
-# if (!dir.exists('venv')) {
-#   system(venvCommand)
-#   system(installCommand)
-#   
-#   # Instalando depend�ncias do R
-#   install.packages("sf")
-# }
-
 # Instalação do pacote renv
 if (!requireNamespace("remotes"))
   install.packages("remotes")
@@ -22,6 +9,5 @@ renv::init()
 # Restaura o ambiente criado
 renv::restore(lockfile = "./renv.lock")
 
-
-# Talvez seja necess�rio editar o working dir
+# Compila o material
 bookdown::render_book("index.Rmd", bookdown::gitbook(lib_dir = "libs"))
